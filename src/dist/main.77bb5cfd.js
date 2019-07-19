55bfe203d16734103d98972ff91c6a961b23c334
Treasure Porth <treasure.rose@gmail.com>
2019-07-18 17:26:47 -0700

    added images, imported more bulma styles

--- src/dist/main.77bb5cfd.js
+++ src/dist/main.77bb5cfd.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/treasureporth/Coding/personal_site/src/images/cityscape.jpg":[["cityscape.3a6a0c07.jpg","images/cityscape.jpg"],"images/cityscape.jpg"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49513" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52124" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
